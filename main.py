981195dfbfd76ca3e07df4842799540471a160fb
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-08-02 11:49:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 # Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
 
+import os
 import requests
 from pyrogram import Client
 
 
 API = "https://api.abirhasan.wtf/google?query="
+
+
+Bot = Client(
+    "PyPi-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+Bot.run()
