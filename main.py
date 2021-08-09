a8d63684362e985dabe40056219fd52bd7239414
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-08-09 10:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,43 @@
 import os
 import requests
 from requests.utils import requote_uri
-from pyrogram import Client
-
+from pyrogram import Client, filters
+from pyrogram.types import *
 
 API = "https://api.abirhasan.wtf/google?query="
 
 
 Bot = Client(
-    "PyPi-Bot",
+    "Google-Search-Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+
+
+@Bot.on_inline_query()
+async def inline(bot, update):
+    results = google(update.query)
+    answers = []
+    for result in results:
+        answers.append(
+            InlineQueryResultArticle(
+                title=result["title"],
+                description=result["description"],
+                input_message_content=InputTextMessageContent(
+                    message_text=result["text"],
+                    disable_web_page_preview=True
+                ),
+                reply_markup=InlineKeyboardMarkup(
+                    InlineKeyboardButton(
+                        text="Link",
+                        url=result["link"]
+                    )
+                )
+            )
+        )
+                
 
 
 def google(query):
@@ -26,7 +51,14 @@ def google(query):
     for info in informations:
         text = f"**Title:** `{info['title']}`"
         text += f"\n**Description:** `{info['description']}`"
-        results.append({"text": text, "link": info['link']})
+        results.append(
+            {
+                "title": info['title'],
+                "description": info['description'],
+                "text": text,
+                "link": info['link']
+            }
+        )
     return results
 
 
